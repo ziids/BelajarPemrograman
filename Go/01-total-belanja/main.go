a0f65253a3d615fac6586e5d611ad5df02f44138
DrelezTM <dreleztm@gmail.com>
2026-06-20 15:33:15 +0700

    fix: logika total dan menambah 'endline'

--- Go/01-total-belanja/main.go
+++ Go/01-total-belanja/main.go
@@ -13,16 +13,22 @@ func main() {
 	fmt.Print("Jumlah Barang 1 = ")
   	fmt.Scan(&qty1)
 
+	fmt.Print("\n")
+
 	fmt.Print("Harga Barang 2 = ")
   	fmt.Scan(&harga2)
 	fmt.Print("Jumlah Barang 2 = ")
   	fmt.Scan(&qty2)
+
+	fmt.Print("\n")
 
 	fmt.Print("Harga Barang 3 = ")
   	fmt.Scan(&harga3)
 	fmt.Print("Jumlah Barang 3 = ")
   	fmt.Scan(&qty3)
 
-	total = harga1 + harga2 + harga3
+	fmt.Print("\n")
+
+	total = (harga1 * qty1) + (harga2 * qty2) + (harga3 * qty3)
 	fmt.Printf("Total Belanja = %v", total)
 }
